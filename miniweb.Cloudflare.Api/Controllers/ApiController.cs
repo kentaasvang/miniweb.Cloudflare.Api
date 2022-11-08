92099485cc097a1288c2d25bff1f81c6b371ee94
Kent Åsvang <github@asvang.no>
2022-11-08 17:44:52 +0100

    inital

---
+++ miniweb.Cloudflare.Api/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace miniweb.Cloudflare.Api.Controllers;
+
+[Route("api")]
+public class ApiController : Controller
+{
+  private readonly ILogger<ApiController> _logger;
+
+  public ApiController(ILogger<ApiController> logger)
+  {
+    _logger = logger;
+  }
+
+  [HttpGet]
+  public string Index()
+  {
+    return "hello";
+  }
+}
---
+++ miniweb.Cloudflare.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace miniweb.Cloudflare.Api.Models;
+
+public class ErrorViewModel
+{
+  public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
