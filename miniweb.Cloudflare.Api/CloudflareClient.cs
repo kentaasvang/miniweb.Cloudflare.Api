be291790929be0f8fe1c7b328f5f8031d7946705
Kent Åsvang <github@asvang.no>
2022-11-13 00:07:30 +0100

    source of truth should be in database, starting to incorporate that

--- miniweb.Cloudflare.Api/CloudflareClient.cs
+++ miniweb.Cloudflare.Api/CloudflareClient.cs
@@ -1,9 +1,10 @@
 using CloudFlare.Client.Api.Zones.DnsRecord;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using miniweb.Cloudflare.Api.Data;
 using CFClient = CloudFlare.Client.CloudFlareClient;
 
 namespace miniweb.Cloudflare.Api;
-
 
 public interface ICloudflareClient
 {
@@ -13,11 +14,14 @@ public interface ICloudflareClient
 public class CloudflareClient : ICloudflareClient
 {
   private readonly ILogger<CFClient> _logger;
+  private readonly ApplicationDbContext _dbContext;
   private readonly CFClient _client;
 
-  public CloudflareClient(ILogger<CFClient> logger, IOptions<CloudflareAuthOptions> options)
+  public CloudflareClient(ILogger<CFClient> logger, IOptions<CloudflareAuthOptions> options,
+    ApplicationDbContext dbContext)
   {
     _logger = logger;
+    _dbContext = dbContext;
     var options1 = options.Value;
     _client = new CFClient(options1.Email, options1.ApiKey);
   }
@@ -25,11 +29,12 @@ public class CloudflareClient : ICloudflareClient
   public async Task<IEnumerable<DnsRecord>> GetAllDnsRecordsAsync()
   {
     var dnsRecords = new List<DnsRecord>();
-    var zones = await _client.Zones.GetAsync();
+    var zoneIds = await (from zone in _dbContext.Zones
+                         select zone.Id).ToListAsync();
 
-    foreach (var zone in zones.Result)
+    foreach (var zoneId in zoneIds)
     {
-      var dns = await _client.Zones.DnsRecords.GetAsync(zone.Id);
+      var dns = await _client.Zones.DnsRecords.GetAsync(zoneId);
 
       if (dns.Success)
         dnsRecords.AddRange(dns.Result);
